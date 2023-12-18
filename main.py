d70d7460053286bfe24e8fc085db636cb33ca4b7
Diamond-Dan <dreti.biz@gmail.com>
2023-12-18 23:33:06 +0000

    fixed error checking for conversion variable on main

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import massMethods
-import volumeMethods
+#import volumeMethods
 
 userinput = 0
-conversion=0
+conversion = 0
 #ask user what orginal measurement value is:
 
-while userinput == 0 or userinput > 5: 
+while userinput == 0 or userinput > 5:
   print('Enter 1 for oz.')
   print('Enter 2 for lb.')
   print('Enter 3 for cups.')
   print('Enter 4 for pint.')
   print('Enter 5 for quart.')
   userinput = int(input('Please enter a unit type.'))
-  #might break if it is a letter 
+  #might break if it is a letter
 #get user input for unit type
 
-#NOTES: method for each converstion 
+#NOTES: method for each converstion
 
 # Convert unit type into grams if mass
-conversion = input("Please enter your amount of substance you have as an integer: ")
-  
-if conversion !=int:
-  print("Please enter a valid integer.")
-  conversion = int(input("Please enter your amount of substance you have as an integer: "))
+while isinstance(conversion, float) == False:
+  try:
+    conversion = float(
+        input(
+            "Please enter your amount of substance you have as an integer or float: "
+        ))
+  except:
+    print("Please enter a valid input.")
 
 # def is_integer(n):
 #   try:
@@ -30,13 +33,13 @@ if conversion !=int:
 #   except ValueError:
 #       return False
 #   else:
-#       return float(n).is_integer() 
+#       return float(n).is_integer()
 
 #send grams to mass, and return desired unit type
 masscalc = massMethods.mass
-volcalc =  volumeMethods.Volume
+#volcalc =  volumeMethods.Volume
 inputmass = masscalc(conversion)
-inputvol = volcalc(conversion)
+#inputvol = volcalc(conversion)
 if userinput == 2:
   print(inputmass.lb_to_oz())
 
