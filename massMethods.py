5848f0201ae7552cf32f13540f1954c83677a75a
Diamond-Dan <dreti.biz@gmail.com>
2023-12-21 15:56:56 +0000

    Added oz_to_lbs and grams_to_lbs methods in mass class

--- massMethods.py
+++ massMethods.py
@@ -1,11 +1,16 @@
 class mass:
-    def __init__(self, mass):
-        self.mass = mass
 
-    def lb_to_oz(self):
-      return(self.mass *16)
+  def __init__(self, mass):
+    self.mass = mass
 
+  def lb_to_oz(self):
+    return (self.mass * 16)
 
-    def lb_to_grams(self):
-      return(self.mass *453.592)
+  def oz_to_lb(self):
+    return (self.mass / 16)
 
+  def lb_to_grams(self):
+    return (self.mass * 453.592)
+
+  def grams_to_lb(self):
+    return (self.mass / 453.592)
